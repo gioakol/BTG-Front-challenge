5b89ba9e79dedfffba80bcc9348626d746d37ff5
Giovanni Beltran <gioakol@gmail.com>
2024-07-30 17:46:22 -0500

    mod: adición de campo de texto para adicionar fondos
    #4 se adiciona campo de texto, validaciones y envio de dato al back

--- src/components/SubscriptionButton.jsx
+++ src/components/SubscriptionButton.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 
-const SubscriptionButton = ({ idClient, idFund, isActive, onUpdate, onNotification }) => {
+const SubscriptionButton = ({ idClient, idFund, isActive, onUpdate, onNotification, amount, minimumAmount }) => {
   const [followed, setFollowed] = useState(isActive);
 
   useEffect(() => {
@@ -23,21 +23,45 @@ const SubscriptionButton = ({ idClient, idFund, isActive, onUpdate, onNotificati
           }
         });
       } else {
+        
+        const amountValue = parseFloat(amount);
+
+        if (isNaN(amountValue) || amountValue < minimumAmount) {
+            onNotification(`El monto debe ser mayor o igual a $${minimumAmount}.`);
+            return;
+        }
+
         response = await axios.post(`${import.meta.env.VITE_API_URL}/transactions/suscribe`, {
           idClient: idClient.toString(),
-          idFund: idFund.toString()
+          idFund: idFund.toString(),
+          investedAmount: amountValue
         }, {
           headers: {
             'Content-Type': 'application/json'
           }
         });
       }
+
       setFollowed(!followed);
       onUpdate();
       onNotification(response.data.message);
     } catch (error) {
-      /*console.error('Error realizando la suscripción/baja:', error);*/
-      onNotification('Error al actualizar el estado de suscripción.');
+      if (error.response) {
+        // Errores con respuesta HTTP
+        switch (error.response.status) {
+          case 401:
+            onNotification(error.response.data.message);
+            break;
+          default:
+            onNotification('Error al actualizar el estado de suscripción.');
+        }
+      } else if (error.request) {
+        // Error al hacer la solicitud
+        onNotification('No se recibió respuesta del servidor.');
+      } else {
+        // Error al configurar la solicitud
+        onNotification('Error al configurar la solicitud.');
+      }
     }
   };
 
